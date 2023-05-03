770a8f4dfef8f3d30b1c1b398d4aeec71ad01f8b
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-05-03 01:45:21 +0100

    Project

---
+++ app.js
@@ -0,0 +1,14 @@
+const API_KEY = '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'
+
+const submitButton = document.querySelector('#submmit') 
+
+async function getMessage()
+console.log('clicked')
+
+try{
+
+} catch {
+
+}
+
+submitButton.addEventListener('click',' getMessage') 
