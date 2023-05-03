e9d8dca58b2b2ef1dd8a2d1b89fd00f22af2d850
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-05-03 03:28:51 +0100

    not yet

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
-const API_KEY = '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'
+const API_KEY = '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'
+const submitButton = document.querySelector('#submit') 
+const outPutElement = document.querySelector('#output')
+const inputElement = document.querySelector('input')
+const historyElement = document.querySelector('.history')
 
-const submitButton = document.querySelector('#submmit') 
 
-async function getMessage()
+async function getMessage() {
 console.log('clicked')
+const options = {
+    method:'POST',
+    headers: { 
+    'Authorization' : `Bearer ${API_KEY}` , 
+    'Content-Type':'application/json'
+    } , 
+  
+body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: "Hello!"}] , 
+        max_tokens: 100
+      })
+       
 
-try{
 
-} catch {
+
+
 
 }
 
-submitButton.addEventListener('click',' getMessage') 
+try{
+
+ const response =  await fetch ('https://api.openai.com/v1/chat/completions', options)
+ const data = await response.json()
+ console.log(data) 
+ outPutElement.textContent = data.choices[0].message.content
+if (data.choices [0].message.content){ 
+    const pElement = document.createElement('p')
+    pElement.textContent = inputElement.value
+    historyElement.append(pElement)
+
+}
+
+
+
+
+
+
+
+} 
+catch (error){
+console.error(error)
+
+}
+}
+
+
+
+submitButton.addEventListener('click', getMessage)  
